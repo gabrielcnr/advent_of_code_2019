d6f69612e8a283af21e0271c6305b859ae618d2d
Gabriel Reis <gabrielcnr@gmail.com>
2019-12-03 14:28:03 +0000

    refactoring tests

--- day03/test_day03.py
+++ day03/test_day03.py
@@ -1,28 +1,40 @@
+import pytest
 from day03 import (closest_intersecting_point, calculate_distance,
                    calculate_intersection_with_fewest_steps)
 
-def test_simple():
-    wire_1 = ['R8', 'U5', 'L5', 'D3']
-    wire_2 = ['U7', 'R6', 'D4', 'L4']
+
+@pytest.mark.parametrize(
+    ['wire_1', 'wire_2', 'distance'],
+    [
+        # case 1
+        [
+            ['R8', 'U5', 'L5', 'D3'],
+            ['U7', 'R6', 'D4', 'L4'],
+            6,
+        ],
+
+        # case 2
+        [
+            ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'],
+            ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83'],
+            159,
+        ],
+
+        # case 3
+        [
+            ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51'],
+            ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7'],
+            135,
+        ],
+
+    ]
+)
+def test_simple(wire_1, wire_2, distance):
     p = closest_intersecting_point(wire_1, wire_2)
-    assert 6 == calculate_distance(p)
+    assert distance == calculate_distance(p)
 
 
 def test_fewest_intersections_simple():
     wire_1 = ['R8', 'U5', 'L5', 'D3']
     wire_2 = ['U7', 'R6', 'D4', 'L4']
-    assert 40 == calculate_intersection_with_fewest_steps(wire_1, wire_2)
-
-
-def test_2():
-    wire_1 = ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72']
-    wire_2 = ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']
-    p = closest_intersecting_point(wire_1, wire_2)
-    assert 159 == calculate_distance(p)
-
-
-def test_3():
-    wire_1 = ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51']
-    wire_2 = ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7']
-    p = closest_intersecting_point(wire_1, wire_2)
-    assert 135 == calculate_distance(p)
+    assert 30 == calculate_intersection_with_fewest_steps(wire_1, wire_2)

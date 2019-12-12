c09d2c1fbf60682967aebb772359f4e33c0e2286
Gabriel Reis <gabrielcnr@gmail.com>
2019-12-12 11:05:41 +0000

    WIP

---
+++ src/aoc19/day11/day11.py
@@ -0,0 +1,87 @@
+import itertools
+from aoc19.day09.day09 import IntCode
+
+
+class Direction:
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+
+class Turn:
+    LEFT = 0
+    RIGHT = 1
+
+
+class Action:
+    PAINT = 'PAINT'
+    TURN = 'TURN'
+
+
+class Color:
+    WHITE = 0
+    BLACK = 1
+
+
+class Robot:
+    def __init__(self, pos=(0, 0), direction=Direction.UP):
+        self.pos = pos
+        self.direction = direction
+        self.panels = {}
+        self.mode = itertools.cycle([Action.PAINT, Action.TURN])
+
+    def run(self, program):
+        program = IntCode(program,
+                          get_input=self.get_input,
+                          output=self.output)
+        program.run()
+
+    def run_from_input(self):
+        program = IntCode.from_input(get_input=self.get_input,
+                                     output=self.output)
+        program.run()
+
+    def get_input(self):
+        return self.panels.get(self.pos, Color.BLACK)
+
+    def output(self, value):
+        mode = next(self.mode)
+        func = getattr(self, mode.lower())
+        func(value)
+
+    def paint(self, color):
+        self.panels[self.pos] = color
+
+    def move(self):
+        d = self.direction
+        x, y = self.pos
+        if d == Direction.UP:
+            self.pos = (x, y + 1)
+        elif d == Direction.RIGHT:
+            self.pos = (x + 1, y)
+        elif d == Direction.DOWN:
+            self.pos = (x, y - 1)
+        elif d == Direction.LEFT:
+            self.pos = (x - 1, y)
+
+    def turn(self, side):
+        if side == Turn.LEFT:
+            incr = -1
+        elif side == Turn.RIGHT:
+            incr = 1
+        else:
+            raise ValueError
+        direction = abs(self.direction + incr) % 4
+        self.direction = direction
+        self.move()
+
+
+def part1():
+    r = Robot()
+    r.run_from_input()
+    return len(r.panels)
+
+
+if __name__ == '__main__':
+    print(part1())
---
+++ src/aoc19/day11/test_day11.py
@@ -0,0 +1,28 @@
+import pytest
+from aoc19.day11.day11 import Robot, Direction, Turn
+
+
+@pytest.mark.parametrize(
+    ['direction', 'expected_pos'],
+    [
+        (Direction.UP, (0, 1)),
+        (Direction.RIGHT, (1, 0)),
+        (Direction.DOWN, (0, -1)),
+        (Direction.LEFT, (-1, 0)),
+    ]
+)
+def test_move(direction, expected_pos):
+    r = Robot(direction=direction)
+    r.move()
+    assert expected_pos == r.pos
+
+
+def test_sanity():
+    r = Robot()
+    for _ in range(4):
+        r.turn(Turn.RIGHT)
+    assert r.pos == (0, 0)
+
+
+def test_example():
+    pass

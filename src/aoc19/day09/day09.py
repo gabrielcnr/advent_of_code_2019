35f5aa453faa4d44512ab9ac71fff7dc59e4e105
Gabriel Reis <gabrielcnr@gmail.com>
2019-12-12 09:33:53 +0000

    Day 9 - part 1

--- src/aoc19/day09/day09.py
+++ src/aoc19/day09/day09.py
@@ -1,4 +1,5 @@
 import operator
+from collections import defaultdict
 
 END = object()
 
@@ -11,7 +12,8 @@ class Mode:
 class IntCode:
     def __init__(self, program, output=None, get_input=None):
         self.pos = 0
-        self.program = program
+        self.program = defaultdict(int)
+        self.program.update({i: n for i, n in enumerate(program)})
         self._output = output
         self._input = get_input
         self._relative_base = 0
@@ -23,6 +25,9 @@ class IntCode:
             contents = fp.read()
         program = [int(n) for n in contents.strip().split(',')]
         return cls(program, **kwargs)
+
+    def p(self, start=None, end=None):
+        return (self.pos, list(self.program.values()))
 
     def run(self):
         self.running = True
@@ -55,14 +60,16 @@ class IntCode:
 
     def opcode_3(self, modes):
         """read input"""
-        p1 = self.read_value(self.pos + 1)
+        p1_mode, _, _ = modes
+        p1 = self.get_index(self.pos + 1, mode=p1_mode)
         self.program[p1] = self.get_input()
+        # import pdb; pdb.set_trace()
         self.pos += 2
 
     def opcode_4(self, modes):
         """send to output"""
-        p1 = self.read_value(self.pos + 1)
-        value = self.program[p1]
+        p1_mode, _, _ = modes
+        value = self.read_value(self.pos + 1, p1_mode)
         self.output(value)
         self.pos += 2
 
@@ -89,25 +96,25 @@ class IntCode:
 
     # TODO: refactor opcodes 7 and 8
     def opcode_7(self, modes):
-        p1_mode, p2_mode, _ = modes
+        p1_mode, p2_mode, p3_mode = modes
         p1 = self.read_value(self.pos + 1, p1_mode)
         p2 = self.read_value(self.pos + 2, p2_mode)
-        idx = self.program[self.pos + 3]
-        self.program[idx] = int(p1 < p2)
+        p3 = self.get_index(self.pos + 3, p3_mode)
+        self.program[p3] = int(p1 < p2)
         self.pos += 4
 
     def opcode_8(self, modes):
-        p1_mode, p2_mode, _ = modes
+        p1_mode, p2_mode, p3_mode = modes
         p1 = self.read_value(self.pos + 1, p1_mode)
         p2 = self.read_value(self.pos + 2, p2_mode)
-        idx = self.program[self.pos + 3]
-        self.program[idx] = int(p1 == p2)
+        p3 = self.get_index(self.pos + 3, p3_mode)
+        self.program[p3] = int(p1 == p2)
         self.pos += 4
 
     def opcode_9(self, modes):
         p1_mode, _, _ = modes
         p1 = self.read_value(self.pos + 1, p1_mode)
-        self._relative_base = p1
+        self._relative_base += p1
         self.pos += 2
 
     def opcode_99(self, modes):
@@ -117,28 +124,47 @@ class IntCode:
         p1_mode, p2_mode, p3_mode = modes
         p1 = self.read_value(self.pos + 1, p1_mode)
         p2 = self.read_value(self.pos + 2, p2_mode)
-        p3 = self.read_value(self.pos + 3)
+        p3 = self.get_index(self.pos + 3, p3_mode)
         self.program[p3] = op(p1, p2)
         self.pos += 4
 
-    def read_value(self, value_or_addr, mode=Mode.IMMEDIATE):
-        if mode == Mode.IMMEDIATE:
-            return value_or_addr
-        elif mode == Mode.POSITION:
-            return self.program[value_or_addr]
-        elif mode == Mode.RELATIVE:
-            return self.program[value_or_addr + self._relative_base]
+    def read_value(self, index, mode):
+        index = self.get_index(index, mode)
+        return self.program[index]
+
+    def get_index(self, index, mode):
+        if mode == 0:#Mode.POSITION:
+            return self.program[index]
+        elif mode == 1:#Mode.IMMEDIATE:
+            return index
+        elif mode == 2:#Mode.RELATIVE:
+            return self._relative_base + self.program[index]
         else:
-            raise RuntimeError(f'unexpected parameter mode: {mode}')
+            return ValueError
+
+    # def read_value(self, pos):
+    #     # value_or_addr = self.program[pos]
+    #     if mode == Mode.IMMEDIATE:
+    #         idx = pos
+    #     elif mode == Mode.POSITION:
+    #         idx = self.program[pos]
+    #     elif mode == Mode.RELATIVE:
+    #         idx = self._relative_base + self.program[pos]# + self._relative_base]
+    #     else:
+    #         raise RuntimeError(f'unexpected parameter mode: {mode}')
+    #
+    #     return self.program[idx]
 
 
-# def part1():
-#     intcode = read_input()
-#     return run_intcode(intcode)
+def part1():
+    L = []
+    program = IntCode.from_input(get_input=lambda: 1, output=L.append)
+    program.run()
+    return L
 
 
 
 
 if __name__ == '__main__':
-    part1()
+    print(part1())
     # part2()
--- src/aoc19/day09/test_day09.py
+++ src/aoc19/day09/test_day09.py
@@ -1,3 +1,5 @@
+import pytest
+
 from day09 import IntCode
 
 
@@ -6,7 +8,15 @@ def test_1():
     L = []
     machine = IntCode(program, output=L.append)
     machine.run()
-    import pdb; pdb.set_trace()
+    assert L == program
+
+
+def test_2():
+    program = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
+    L = []
+    machine = IntCode(program, output=L.append)
+    machine.run()
+    assert len(str(L[0])) == 16
 
 
 def test_3():
@@ -15,3 +25,34 @@ def test_3():
     machine = IntCode(program, output=L.append)
     machine.run()
     assert L[0] == 1125899906842624
+
+
+def test_day5():
+    program = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+    L = []
+    machine = IntCode(program, output=L.append, get_input=lambda: 8)
+    machine.run()
+    assert L[0] == 1
+
+
+@pytest.mark.parametrize(
+    ['in_', 'out'],
+    [
+        (8, 1000),
+        (10, 1001),
+        (5, 999),
+    ]
+
+)
+def test_day5_2(in_, out):
+    program = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+               1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+               999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+
+
+    L = []
+    machine = IntCode(program, output=L.append, get_input=lambda: in_)
+    machine.run()
+    output, = L
+    assert output == out
+

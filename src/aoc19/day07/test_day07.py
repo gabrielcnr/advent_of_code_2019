ba31478adb8fec0ba414933270140e8681dded86
Gabriel Reis <gabrielcnr@gmail.com>
2019-12-12 09:36:56 +0000

    WIP - test for part 2

--- src/aoc19/day07/test_day07.py
+++ src/aoc19/day07/test_day07.py
@@ -19,3 +19,10 @@ def test_get_signal_sent_to_thrusters_3():
     intcode = [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33,
                1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0]
     assert 65210 == get_max_thruster_signal(phase_setting_sequence, intcode)
+
+
+def test_part2_feedback_loop():
+    phase_setting_sequence = '98765'
+    intcode = [3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26,
+               27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5]
+    assert 139629729 == get_max_thruster_signal(phase_setting_sequence, intcode)
